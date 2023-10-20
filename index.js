03ee8bdd1dcfa2178f04836fc52b83e6cf7238c4
Salvia <nicolas-ezequiel.salvia@capgemini.com>
2023-10-20 15:48:43 -0300

    Se agrega ruta /

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const express = require('express')
+const nodeFetch = require('node-fetch')
+const app = express()
+const port = 3000
+
+app.get('/', async (req, res) => {
+    const response = await fetch('https://jsonplaceholder.org/posts');
+    const data = await response.json(response);
+
+    res.send(data);
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
 
 console.log("Hola Mundo")
