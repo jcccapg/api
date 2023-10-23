6a2a557058fe1810a1bf15c9b4fad4b7df5ea3dc
Cancela <juan-carlos.cancela@capgemini.com>
2023-10-23 15:59:51 -0300

    added node-fetch to proj

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response } from 'express'
+import nodeFetch from 'node-fetch'
 const app = express()
 const port = 3000
 
 app.get('/', async (_req: Request, res: Response) => {
-    const response = await fetch('https://jsonplaceholder.org/posts');
+    const response = await nodeFetch('https://jsonplaceholder.org/posts');
     const data = await response.json();
-
     res.send(data);
 })
 
